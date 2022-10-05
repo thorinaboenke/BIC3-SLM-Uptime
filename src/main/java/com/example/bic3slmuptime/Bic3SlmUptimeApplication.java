f463f515fe2c65e7e71f150e101f01cdc892a913
Thorina Boenke <thorina-boenke@web.de>
2022-10-05 19:23:19 +0200

    initial commit

---
+++ src/main/java/com/example/bic3slmuptime/Bic3SlmUptimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.bic3slmuptime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bic3SlmUptimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bic3SlmUptimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bic3slmuptime/Bic3SlmUptimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bic3slmuptime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bic3SlmUptimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
